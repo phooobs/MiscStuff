30e16571b54fd948fc8c30e219364e15eabf9493
Ferguson <jwferguson@fortlewis.edu>
2020-09-15 10:52:45 -0600

    worked on joystick

--- Joystick_Stepper/Joystick_Stepper.ino
+++ Joystick_Stepper/Joystick_Stepper.ino
@@ -1,12 +1,40 @@
-const int joystickPin = A0;
+#include <SoftwareSerial.h>
+
+const int joystickPin = A7;
+const int CMDPin = A7;
+SoftwareSerial BlueSerial(2, 3); // RX, TX
 
 void setup() {
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
+
+  BlueSerial.begin(9600);
+  while (!BlueSerial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  BlueSerial.println("AT+TID=2222222222");
+
+  if (BlueSerial.available() > 0) {
+    String incomingCommand = Serial.readString();
+    Serial.println(incomingCommand);
+  }
+  
+  BlueSerial.println("AT+RID=1111111111");
+  
+  if (BlueSerial.available() > 0) {
+    String incomingCommand = Serial.readString();
+    Serial.println(incomingCommand);
+  }
 }
 
 void loop() {
-  Serial.println(analogRead(joystickPin));
+
+  Serial.print(analogRead(joystickPin));
+  if (BlueSerial.available() > 0) {
+    String incomingCommand = Serial.readString();
+    //Serial.print(" ");
+    Serial.println(incomingCommand);
+  }
 }

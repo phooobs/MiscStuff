44e74f34ce208e73993bd6d16eaad6f68aa4b7f8
James Ferguson <phooobs@gmail.com>
2020-10-09 23:29:42 -0600

    made some progress on SPI ovehal trying to reset crashes it sorta

--- LCD_Temperature/LCD_Temperature.ino
+++ LCD_Temperature/LCD_Temperature.ino
@@ -6,6 +6,16 @@
 #include <Adafruit_SSD1306.h>
 
 #define ONE_WIRE_BUS 2
+#define UP_BUTTON 3
+#define SELECT_BUTTON 4
+#define DOWN_BUTTON 5
+
+bool upButtonState = false;
+bool lastUpButtonState = false;
+bool selectButtonState = false;
+bool lastSelectButtonState = false;
+bool downButtonState = false;
+bool lastDownButtonState = false;
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature temperatureSensor(&oneWire);
@@ -18,13 +28,16 @@ enum States {LOGGING, WIFI_CONFIG};
 
 // SPI commands
 const byte SIMO_TEMPERATURE = B00000001;
-const byte MISO_WIFI = B00000010;
-const byte SIMO_WIFI = B00000011;
+const byte SIMO_CONNECT_WIFI = B00000010;
+const byte SIMO_ACCESS_POINT = B00000011;
+const byte SIMO_PASSWORD = B00000100;
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 float temperature = 0;
+
+
 
 void setup() {
   Serial.begin(9600);
@@ -43,41 +56,144 @@ void setup() {
 }
 
 void loop() {
+  upButtonState = digitalRead(UP_BUTTON);
+  selectButtonState = digitalRead(SELECT_BUTTON);
+  downButtonState = digitalRead(DOWN_BUTTON);
+
   static States state;
   
   switch(state) {
     case LOGGING :
-      temperatureSensor.requestTemperatures();
-      temperature = temperatureSensor.getTempCByIndex(0);
-
-      // send temperature data to slave
-      digitalWrite(SS, LOW); // enable Slave Select
-      SPI.transfer(SIMO_TEMPERATURE);
-      String sendData = String(temperature);
-      for (unsigned int i = 0; i < sendData.length(); i++) {
-        SPI.transfer(sendData[i]);
-      }
-      SPI.transfer('\r');
-      digitalWrite(SS, HIGH); // disable Slave Select
-      
-      display.clearDisplay();
-      display.setTextSize(1);      // Normal 1:1 pixel scale
-      display.setTextColor(SSD1306_WHITE); // Draw white text
-      display.setCursor(0, 0);     // Start at top-left corner
-      display.cp437(true);         // Use full 256 char 'Code Page 437' font
-      display.print("Temperature: ");
-      display.print(temperature);
-      display.print(char(248)); // degree charicter
-      display.print("C");
-      display.display();
+      state = logging();
       break;
       
     case WIFI_CONFIG :
-      state = TRANSFER_TEMPERATURE;
+      state = wifiConfig();
       break;
 
     default :
-      state = TRANSFER_TEMPERATURE;
+      state = LOGGING;
       break;
   }
+  lastUpButtonState = upButtonState;
+  lastSelectButtonState = selectButtonState;
+  lastDownButtonState = downButtonState;
+}
+
+States logging () {
+
+  if ((selectButtonState == true) && (lastSelectButtonState == false)) { // select button pressed switch to config menu
+    return WIFI_CONFIG;
+  }
+  
+  temperatureSensor.requestTemperatures();
+  temperature = temperatureSensor.getTempCByIndex(0);
+
+  // send temperature data to slave
+  digitalWrite(SS, LOW); // enable Slave Select
+  SPI.transfer(SIMO_TEMPERATURE);
+  String sendData = String(temperature);
+  for (unsigned int i = 0; i < sendData.length(); i++) {
+    SPI.transfer(sendData[i]);
+  }
+  SPI.transfer('\r');
+  digitalWrite(SS, HIGH); // disable Slave Select
+      
+  display.clearDisplay();
+  display.drawRect(0, 0, SCREEN_WIDTH, 11, SSD1306_WHITE);
+  display.setTextSize(1);      // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE); // Draw white text
+  display.setCursor(2, 2);     // Start at top-left corner
+  display.cp437(true);         // Use full 256 char 'Code Page 437' font
+  display.print("Temperature: ");
+  display.print(temperature);
+  display.print(char(248)); // degree charicter
+  display.print("C");
+  display.display();
+
+  return LOGGING;
+}
+
+States wifiConfig () {
+  enum MenuItem {BACK, ACCESS_POINT, PASSWORD, APPLY, END};
+  const String menuItemText[] = {"Back", "Set Access Point", "Set Password", "Apply"};
+  static MenuItem menuPosition;
+
+  if ((upButtonState == true) && (lastUpButtonState == false)) { // menu up
+    if (menuPosition == 0) {
+      menuPosition = END - 1;
+    } else {
+      menuPosition = menuPosition - 1;
+    }
+    
+  } else if ((downButtonState == true) && (lastDownButtonState == false)) { // menu down
+    if (menuPosition == END - 1) {
+      menuPosition = 0;
+    } else {
+      menuPosition = menuPosition + 1;
+    }
+    
+  } else if ((selectButtonState == true) && (lastSelectButtonState == false)) { // select button pressed
+    switch(menuPosition) {
+      case BACK :
+        return LOGGING;
+        break;
+        
+      case ACCESS_POINT : {
+        Serial.println("Set Access Point");
+
+        // send temperature data to slave
+        digitalWrite(SS, LOW); // enable Slave Select
+        SPI.transfer(SIMO_ACCESS_POINT);
+        String sendData = "Guest";
+        for (unsigned int i = 0; i < sendData.length(); i++) {
+          SPI.transfer(sendData[i]);
+        }
+        SPI.transfer('\r');
+        digitalWrite(SS, HIGH); // disable Slave Select
+        break; }
+  
+      case PASSWORD : {
+        Serial.println("Set Password");
+
+        // send temperature data to slave
+        digitalWrite(SS, LOW); // enable Slave Select
+        SPI.transfer(SIMO_PASSWORD);
+        String sendData = "";
+        for (unsigned int i = 0; i < sendData.length(); i++) {
+          SPI.transfer(sendData[i]);
+        }
+        SPI.transfer('\r');
+        digitalWrite(SS, HIGH); // disable Slave Select
+        break; }
+
+      case APPLY : {
+        Serial.println("Apply");
+        // send temperature data to slave
+        digitalWrite(SS, LOW); // enable Slave Select
+        SPI.transfer(SIMO_CONNECT_WIFI);
+        digitalWrite(SS, HIGH); // disable Slave Select
+        break; }
+    }
+  }
+  
+  display.clearDisplay();
+  display.setTextSize(1);      // Normal 1:1 pixel scale
+
+  
+  
+  for (MenuItem i = 0; i < END; i = i + 1) {
+    display.setCursor(2, 2 + i * 10);     // Start at top-left corner
+    if (menuPosition == i) {
+      display.setTextColor(SSD1306_WHITE); // selected item, Draw Black text
+      display.drawRect(0, i*10, SCREEN_WIDTH, 11, SSD1306_WHITE);
+    } else {
+      display.setTextColor(SSD1306_WHITE); // not selected item, Draw white text
+    }
+    display.print(menuItemText[i]);
+  }
+  
+  display.display();
+
+  return WIFI_CONFIG;
 }
--- LCD_Temperature_Slave/LCD_Temperature_Slave.ino
+++ LCD_Temperature_Slave/LCD_Temperature_Slave.ino
@@ -1,46 +1,49 @@
 #include <SPI.h>
 volatile boolean process;
 volatile float temperature;
+volatile boolean updateWiFi;
 
 // commands
 const byte SIMO_TEMPERATURE = B00000001;
-const byte MISO_WIFI = B00000010;
-const byte SIMO_WIFI = B00000011;
+const byte SIMO_CONNECT_WIFI = B00000010;
+const byte SIMO_ACCESS_POINT = B00000011;
+const byte SIMO_PASSWORD = B00000100;
 
-String AP = "Guest";       // AP NAME. Change it to the AP you are using. You can even use the your smartphone's hotspot. 
-String PASS = ""; // AP PASSWORD. At FLC, the Guest AP doesn't need a password
+// WIFI data
+String accessPoint = "Test";       // AP NAME. Change it to the AP you are using. You can even use the your smartphone's hotspot. 
+String password = ""; // AP PASSWORD. At FLC, the Guest AP doesn't need a password
 String API = "GVDN5WA12ZF3ZCU3"; // Must be your ThingSpeak channel's API. 
-String HOST = "api.thingspeak.com";
-String PORT = "80";
+String Host = "api.thingspeak.com";
+String Port = "80";
 int countTrueCommand;
 int countTimeCommand; 
 boolean found = false; 
 
-
- 
 void setup (void) {
-  // ESP8266 setup
-  
   Serial.begin(115200); // ESP setup 
-  sendCommand("AT+RST",5,"OK");
-  sendCommand("AT",5,"OK");
-  sendCommand("AT+CWMODE=1",5,"OK");
-  String cmd = F("AT+CWJAP=\"");
-  cmd += AP;
-  cmd += F("\",\"");
-  cmd += PASS;
-  cmd += F("\"");
-  sendCommand(cmd,20,"OK");
-
+  
+  connectWIFI();
 
   pinMode(MISO, OUTPUT); // have to send on master in so it set as output
   SPCR |= _BV(SPE); // turn on SPI in slave mode
   process = false;
+  updateWiFi = false;
   SPI.attachInterrupt(); // turn on interrupt
+}
+
+void connectWIFI () { // ESP8266 setup
+  sendCommand("AT+RST",5,"OK");
+  sendCommand("AT",5,"OK");
+  sendCommand("AT+CWMODE=1",5,"OK");
+  String cmd = F("AT+CWJAP=\"");
+  cmd += accessPoint;
+  cmd += F("\",\"");
+  cmd += password;
+  cmd += F("\"");
+  sendCommand(cmd,20,"OK");
 }
  
 ISR (SPI_STC_vect) { // SPI interrupt routine
-
   static char buff [50];
   static byte indx;
   
@@ -57,12 +60,27 @@ ISR (SPI_STC_vect) { // SPI interrupt routine
         curentCommand = B00000000;
       }
     }
-  } else if (curentCommand == MISO_WIFI) {
-
+  } else if (curentCommand == SIMO_CONNECT_WIFI) {
+    updateWiFi = true;
     curentCommand = B00000000;
-  } else if (curentCommand == SIMO_WIFI) {
-
-    curentCommand = B00000000;
+  } else if (curentCommand ==  SIMO_ACCESS_POINT) {
+    if (indx < sizeof buff) {
+      buff [indx++] = recivedByte; // save data in the next index in the array buff
+      if (recivedByte == '\r') { //check for the end of the word
+        accessPoint = String(buff);
+        indx = 0;
+        curentCommand = B00000000;
+      }
+    }
+  } else if (curentCommand ==  SIMO_PASSWORD) {
+    if (indx < sizeof buff) {
+      buff [indx++] = recivedByte; // save data in the next index in the array buff
+      if (recivedByte == '\r') { //check for the end of the word
+        password = String(buff);
+        indx = 0;
+        curentCommand = B00000000;
+      }
+    }
   } else { // we dont have a curent command, the recived byte is the command
     curentCommand = recivedByte;
   }
@@ -75,10 +93,17 @@ void loop (void) {
     String sensorByteStr = String(temperature);  
     String getData = "GET /update?api_key="+ API +"&field1="+sensorByteStr; // send to ThingSpeak, needs to be a string
     sendCommand("AT+CIPMUX=1",5,"OK");
-    sendCommand("AT+CIPSTART=0,\"TCP\",\""+ HOST +"\","+ PORT,15,"OK");
+    sendCommand("AT+CIPSTART=0,\"TCP\",\""+ Host +"\","+ Port,15,"OK");
     sendCommand("AT+CIPSEND=0," + String(getData.length()+4),4,">");
     Serial.println(getData);
     sendCommand("AT+CIPCLOSE=0",5,"OK");
+  }
+
+  if (updateWiFi) {
+    updateWiFi = false;
+    digitalWrite(13, HIGH);
+    delay(500);
+    digitalWrite(13, LOW);
   }
 }
 
